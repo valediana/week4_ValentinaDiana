be18f8c7d9d18511cae5aa2b9f632d9c7df4cda8
valediana <valentinadiana94@yahoo.it>
2021-11-01 11:29:34 +0100

    sito film con alcune correzioni

--- ScriptFilm.js
+++ ScriptFilm.js
@@ -1,100 +1,47 @@
-let loggedIn = false;
-
-function openForm() {
-    let form = document.getElementById("myForm")
-    form.style.display = "block";
-}
-
-function closeForm() {
-    let form = document.getElementById("myForm")
-    form.style.display = "none";
-}
-
-function welcome() {
-
-    //se l'utente è loggato:
-    if (loggedIn) {
-        let username = localStorage.getItem("username");
-        alert("Benvenuto " + username);
+function benvenuto() {
+    let username = localStorage.getItem('nomeInserito');
+    if (username != null && indirizzo!=null) {
+        alert("Benvenuto" + username);
     }
     else {
-        alert("Benvenuto!")
+        alert("Benvenuto");
+    }
+}
+function checkRegistrazione() {
+    let username = localStorage.getItem('nomeInserito');
+    let emailInserita = localStorage.get('indirizzo');
+    if (username != null && emailInserita != null) {
+        disiscriviti();
+    }
+    else {
+        mostraForm();
+
     }
 }
 
 
-//verificare che l'utente ha inserito qualcosa
-function changeLoginPage() {
-    //recupero gli elementi il cui class name è input
-    let inputs = document.getElementsByClassName("input");
 
-    //recuper il tasto login
-    let loginButton = document.querySelector("input[type='submit']")
+function mostraForm() {
+    let form = document.getItemById('myForm');
+    form.style.display = 'block';
+}
+function chiudiForm() {
+    let form = document.getItemById('myForm');
+    form.style.display = 'none';
 
-    //verifico che siano compilati
-    for (let i = 0; i < inputs.length; i++) {
-
-        if (inputs[i].value == "") //se l'i-esimo input è stringa vuota
-        {
-            //tasto login resta disabilitato
-            loginButton.disabled = true; //oppure disabled = "disabled"
-            return;
-        }
-        loginButton.disabled = false; //tasto login abilitato
-    }
-
-    
+    //ora mostro bottone unsubscribe
+    let cancella = document.getItemById('disiscriviti');
+    cancella.style.display = 'block';
+}
+function salva() {
+    let nomeInserito = document.getItemById('usernameInput');
+    let indirizzo = document.getItemById('email');
+    localStorage.setItem('username', nomeInserito);
+    localStorage.setItem('email', indirizzo);
 }
 
-//al submit, salvo i dati inseriti dall'utente
-function save() {
-    //recupero tutti gli elementi che hanno class name input -> inputs = array di 2 elementi
-    
-    let inputs = document.getElementsByClassName("input");
-
-    let insUsername = inputs[0].value;
-    //salvo nel local storage
-    localStorage.setItem('username', insUsername);
-   
-}
-
-function hideForm() {
-    ////recupero la form
-    let form = document.querySelector("form");
-
-    ////nascondo la form
-    form.style.display = "none"; //cambio stile della form con display:none;
-
-    //recupero tasto di logout
-    let logoutButton = document.getElementById("logoutButton");
-
-    //lo rendo visibile
-    logoutButton.style.display = "inline";
-}
-
-function checkLoggedUser() {
-    //recupero eventuali dati dal local storage
-    let storedUsername = localStorage.getItem('username');
-   
-
-    //se ci sono (ovvero l'utente è loggato) nascondo la form
-    if (storedUsername != null) {
-        hideForm();
-    }
-
-    //altrimenti niente
-}
-
-function logout() {
-    localStorage.clear() //svuoto il local storage
-
-    //recupero la form
-    let form = document.querySelector("form");
-
-    //mostro la form
-    form.style.display = "inline"; 
-
-    //recupero e nascondo il tasto di logout 
-    let logoutButton = document.getElementById("logoutButton");
-    logoutButton.style.display = "none";
+function disiscriviti() {
+    localStorage.clear();
+    let iscriviti = document.getItemById('iscriviti');
+    iscriviti.style.display = 'block';
 }
